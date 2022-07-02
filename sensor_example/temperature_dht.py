b19a94f100008167826e70c7aa12cbd56b419780
Laoa Raoand Jalal <laoa99@outlook.com>
2022-07-02 11:40:25 +0200

    init

---
+++ sensor_example/temperature_dht.py
@@ -0,0 +1,23 @@
+# Data is sent to Pybytes. Needs to flashed with Pybyte firmware
+import time
+from machine import Pin
+from lib.dht import DHT # https://github.com/JurassicPork/DHT_PyCom
+
+# Type 0 = dht11
+# Type 1 = dht22
+
+th = DHT(Pin('P10', mode=Pin.OPEN_DRAIN), 0)
+time.sleep(2)
+
+print("hello")
+while True:
+    result = th.read()
+    while not result.is_valid():
+        time.sleep(.5)
+        result = th.read()
+    print('Temp:', result.temperature)
+    print('RH:', result.humidity)
+    #pybytes.send_signal(1,result.temperature)
+    #pybytes.send_signal(2,result.humidity)
+
+    time.sleep(5)
